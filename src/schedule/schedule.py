5e20a79e3fcf10f9ca902f7fa697881db693a2cc
Stephan Richter <stephan.richter@gmail.com>
2014-09-20 16:13:30 -0400

    The script now works completely. Just waiting for Susanne to update name-eail mapping.

--- src/schedule/schedule.py
+++ src/schedule/schedule.py
@@ -6,38 +6,78 @@ import optparse
 import urllib
 
 import gdata.calendar.service
+import gdata.spreadsheet.service
 import atom
 
-SESSION_FILENAME = 'session.data'
+CAL_SESSION_FILENAME = 'cal_session.data'
+DOCS_SESSION_FILENAME = 'doc_session.data'
 
 class Schedule(object):
 
+    DOC_ID = '1s9mfuxESgiXIxzGHVysGg64hH4bVlpT3JkaTdGWDJwA'
     CALENDAR_ID = '8rljkisdsimoemo5r22vv7fou4%40group.calendar.google.com'
 
-    SCOPE = ['http://www.google.com/calendar/feeds/',
-             'https://www.google.com/calendar/feeds/']
+    CAL_SCOPE = ['http://www.google.com/calendar/feeds/',
+                 'https://www.google.com/calendar/feeds/']
+    DOCS_SCOPE = ['http://spreadsheets.google.com/feeds/',
+                  'https://spreadsheets.google.com/feeds/']
 
     def __init__(self, options):
         self.options = options
-        self.setup_service()
+        self.setup_cal_service()
+        self.setup_docs_service()
 
-    def setup_service(self):
-        if os.path.exists(SESSION_FILENAME) and not self.options.new_session:
-            with open(SESSION_FILENAME, 'r') as file:
-                self.srv = pickle.load(file)
+    def setup_docs_service(self):
+        if os.path.exists(DOCS_SESSION_FILENAME) and \
+          not self.options.new_session:
+            with open(DOCS_SESSION_FILENAME, 'r') as file:
+                self.docs_srv = pickle.load(file)
         else:
             next = 'http://www.germansaturdayschoolboston.org/bad.html'
             secure = False
             session = True
-            self.srv = gdata.calendar.service.CalendarService()
-            url = self.srv.GenerateAuthSubURL(next, self.SCOPE, secure, session)
+            self.docs_srv = gdata.spreadsheet.service.SpreadsheetsService()
+            url = self.docs_srv.GenerateAuthSubURL(
+                next, self.DOCS_SCOPE, secure, session)
             print 'Please visit this URL to create a token:'
             print url
             token = urllib.unquote(raw_input('Token: '))
-            self.srv.SetAuthSubToken(token)
-            self.srv.UpgradeToSessionToken()
-            with open(SESSION_FILENAME, 'w') as file:
+            self.docs_srv.SetAuthSubToken(token)
+            self.docs_srv.UpgradeToSessionToken()
+            with open(DOCS_SESSION_FILENAME, 'w') as file:
+                pickle.dump(self.docs_srv, file)
+
+    def setup_cal_service(self):
+        if os.path.exists(CAL_SESSION_FILENAME) and \
+          not self.options.new_session:
+            with open(CAL_SESSION_FILENAME, 'r') as file:
+                self.cal_srv = pickle.load(file)
+        else:
+            next = 'http://www.germansaturdayschoolboston.org/bad.html'
+            secure = False
+            session = True
+            self.cal_srv = gdata.calendar.service.CalendarService()
+            url = self.cal_srv.GenerateAuthSubURL(
+                next, self.CAL_SCOPE, secure, session)
+            print 'Please visit this URL to create a token:'
+            print url
+            token = urllib.unquote(raw_input('Token: '))
+            self.cal_srv.SetAuthSubToken(token)
+            self.cal_srv.UpgradeToSessionToken()
+            with open(CAL_SESSION_FILENAME, 'w') as file:
                 pickle.dump(self.srv, file)
+
+    def create_name_email_map(self):
+        self.name_email_map = {}
+        query = gdata.spreadsheet.service.CellQuery()
+        query.min_row = '2'
+
+        rows = self.docs_srv.GetListFeed(
+            self.DOC_ID, self.sheets['E-mails'], query=query)
+
+        for row in rows.entry:
+            data = row.custom
+            self.name_email_map[data['name'].text] = data['e-mail'].text
 
     def add_event(self, title, content, start_time, end_time, guests):
         event = gdata.calendar.CalendarEventEntry()
@@ -52,24 +92,49 @@ class Schedule(object):
         for guest in guests:
             event.who.append(gdata.calendar.Who(email=guest))
 
-        new_event = self.srv.InsertEvent(
+        new_event = self.cal_srv.InsertEvent(
             event, '/calendar/feeds/%s/private/full' % self.CALENDAR_ID)
 
     def create(self):
-        feed = self.srv.GetCalendarListFeed()
-        #for i, a_calendar in enumerate(feed.entry):
-        #    print '\t%s. %s' % (i, a_calendar.title.text,)
-        self.add_event(
-            'Test',
-            datetime.datetime(2014, 9, 20, 14, 30),
-            datetime.datetime(2014, 9, 20, 15, 0),
-            ['stephan.richter@gmail.com']
-            )
+        wss = self.docs_srv.GetWorksheetsFeed(key=self.DOC_ID)
+        self.sheets = {
+            ws.title.text: ws.id.text.split('/')[-1]
+            for ws in wss.entry}
+        self.create_name_email_map()
+
+        query = gdata.spreadsheet.service.CellQuery()
+        query.min_row = '3'
+
+        rows = self.docs_srv.GetListFeed(
+            self.DOC_ID, self.sheets['Schedule'], query=query)
+
+        for row in rows.entry[1:]:
+            data = row.custom
+            date = datetime.datetime.strptime(
+                data['date'].text, '%B %d, %Y').date()
+
+            for task, start, end in [
+                    ('setup', datetime.time(8, 30), datetime.time(9, 0)),
+                    ('computer', datetime.time(9, 0), datetime.time(12, 30)),
+                    ('shelving', datetime.time(9, 0), datetime.time(12, 30))]:
+                start_dt = datetime.datetime.combine(date, start)
+                end_dt = datetime.datetime.combine(date, end)
+                guests = filter(None,
+                    [self.name_email_map.get(name.split(' ')[0].strip())
+                     for name in (data[task].text or '').split(', ')]
+                    )
+                print task.title()
+                print start_dt, end_dt
+                print guests
+                print
+                self.add_event(
+                   task.title(), data[task].text or '',
+                   start_dt, end_dt, guests)
 
 ###############################################################################
 # Command-line UI
 
-parser = optparse.OptionParser("%prog <google-doc>")
+parser = optparse.OptionParser("%prog [options]")
 
 config = optparse.OptionGroup(
     parser, "Configuration", "Options to configure the scheduler.")
