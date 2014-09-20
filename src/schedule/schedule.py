4a47b0fbcd31750ac0dcc8d40a36942ebaa443ba
Stephan Richter <stephan.richter@gmail.com>
2014-09-20 12:11:03 -0400

    At least got authentication to work. Now on to better things.

--- src/schedule/schedule.py
+++ src/schedule/schedule.py
@@ -1,16 +1,24 @@
 import sys
 import optparse
+import urllib
 
-import gdata.spreadsheet.service
+import gdata.calendar.service
 
-def create_schedule(doc_id, username, password):
-    client = gdata.spreadsheet.service.SpreadsheetsService()
-    client.debug = True # feel free to toggle this
-    client.email = username
-    client.password = password
-    client.source = 'GSSBLib Scheduler'
-    client.ProgrammaticLogin()
-    import pdb; pdb.set_trace()
+def create_schedule(doc_id):
+    next = 'http://www.germansaturdayschoolboston.org/bad.html'
+    scope = ['http://www.google.com/calendar/feeds/',
+             'https://www.google.com/calendar/feeds/']
+    secure = False
+    session = True
+    srv = gdata.calendar.service.CalendarService()
+    url = srv.GenerateAuthSubURL(next, scope, secure, session)
+    print url
+    token = raw_input('Token: ')
+    srv.SetAuthSubToken(urllib.unquote(token))
+    srv.UpgradeToSessionToken()
+    feed = srv.GetCalendarListFeed()
+    for i, a_calendar in enumerate(feed.entry):
+        print '\t%s. %s' % (i, a_calendar.title.text,)
 
 ###############################################################################
 # Command-line UI
@@ -20,22 +28,14 @@ parser = optparse.OptionParser("%prog <google-doc>")
 config = optparse.OptionGroup(
     parser, "Configuration", "Options to configure the scheduler.")
 
-config.add_option(
-    '--username', '--user', action="store", dest='username',
-    help="""Username to access the Google account.""")
-
-config.add_option(
-    '--password', '--pwd', action="store", dest='password',
-    help="""Password to access the Google account.""")
-
 parser.add_option_group(config)
 
 def post_schedule(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
-    options, args = parser.parse_args(args)
-    create_schedule(args[0], options.username, options.password)
+    options, args = parser.parse_args(argv)
+    create_schedule(args[0])
 
 # Command-line UI
 ###############################################################################
